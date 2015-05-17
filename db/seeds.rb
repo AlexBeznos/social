81abbac19845b20570e89a2b3c4d4ee5c9558455
Alex Beznos <beznosa@yahoo.com>
2015-05-18 02:09:39 +0300

    change seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,3 +2,5 @@ User.create(email: 'a@u.com', password: 'qwerty', password_confirmation: 'qwerty
 User.create(email: 'b@u.com', password: 'qwerty', password_confirmation: 'qwerty')
 User.create(email: 'c@u.com', password: 'qwerty', password_confirmation: 'qwerty')
 User.create(email: 'd@u.com', password: 'qwerty', password_confirmation: 'qwerty')
+User.create(email: 'alex@u.com', password: 'qwerty', password_confirmation: 'qwerty', group: 'admin')
+User.create(email: 'bbking@u.com', password: 'qwerty', password_confirmation: 'qwerty', group: 'admin')
