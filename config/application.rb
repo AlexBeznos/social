b8eb42d2461c29aadc7b0a4ff58b58ecc884307c
Alex Beznos <beznosa@yahoo.com>
2015-05-18 04:01:57 +0300

    fonts fix

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module Social
 
     Rails.application.config.assets.precompile += %w( *.css )
     Rails.application.config.assets.precompile += %w( *.js )
+    config.assets.precompile << /\.(?:svg|eot|woff|ttf)\z/
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
