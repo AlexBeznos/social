ba1c314f96b1bc4a133793558574b0686676dd3c
Alex Beznos <beznosa@yahoo.com>
2015-05-19 19:57:14 +0300

    preinstall for internationalization

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,9 @@ module Social
     Rails.application.config.assets.precompile += %w( *.css )
     Rails.application.config.assets.precompile += %w( *.js )
     config.assets.precompile << /\.(?:svg|eot|woff|ttf|jpg|png)\z/
+    config.i18n.load_path += Dir[Rails.root.join('config/locales/**/*.{rb,yml}').to_s]
+    config.i18n.default_locale = :ru
+    config.i18n.available_locales = [:en, :ru, :ua]
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
