94b05a0211700056778946f67b7dfe78754171c1
Alex Beznos <beznosa@yahoo.com>
2015-06-11 10:39:50 +0300

    add show page for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   before_filter :require_franchisee
+  before_action :find_user, except: [:index, :new, :create]
+  before_filter :require_proper_franchisee, except: [:index, :new, :create]
 
   def index
     @users = current_user.place_owners
@@ -8,12 +10,27 @@ class UsersController < ApplicationController
   def show
   end
 
+  def destroy
+    @user.destroy
+    redirect_to users_path
+  end
+
   private
     def require_franchisee
       if !current_user
         no_access_to_login
       elsif ![:franchisee, :admin].include?(current_user.group.to_sym)
         no_access_to_place
+      end
+    end
+
+    def find_user
+      @user = User.find(params[:id])
+    end
+
+    def require_proper_franchisee
+      unless current_user.place_owners.include?(@user)
+        redirect_to users_path, alert: 'You have no rights to access this page!'
       end
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,7 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :users, only: [:index, :show]
+  resources :users
 
   resources :places do
     resources :messages, except: [:index, :show]
