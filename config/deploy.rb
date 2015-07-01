85766c496cea1c9ef676507b7e2fedc2d1becd7d
Alex Beznos <beznosa@yahoo.com>
2015-07-01 23:32:43 +0300

    unicorn, mina stage setup

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,9 @@
 require 'mina/bundler'
 require 'mina/rails'
 require 'mina/git'
-# require 'mina/rbenv'  # for rbenv support. (http://rbenv.org)
-require 'mina/rvm'    # for rvm support. (http://rvm.io)
+require 'mina/rvm'
+require 'mina_sidekiq/tasks'
+require 'mina/unicorn'
 
 # Basic settings:
 #   domain       - The hostname to SSH to.
@@ -11,45 +12,63 @@ require 'mina/rvm'    # for rvm support. (http://rvm.io)
 #   branch       - Branch name to deploy. (needed by mina/git)
 
 set :domain, 'stage.gofriends.com.ua'
-set :user, 'root'
-set :port, '22'
-set :term_mode, nil
-set :deploy_to, '/home/deployer/rails'
+set :deploy_to, '/home/deployer/social/'
 set :repository, 'git@github.com:AlexBeznos/social.git'
-set :branch, 'master'
-
-# For system-wide RVM install.
-#   set :rvm_path, '/usr/local/rvm/bin/rvm'
+set :branch, 'mina'
+set :user, 'deployer'
+set :forward_agent, true
+set :port, '22'
+set :unicorn_pid, "#{deploy_to}/shared/pids/unicorn.pid"
 
 # Manually create these paths in shared/ (eg: shared/config/database.yml) in your server.
 # They will be linked in the 'deploy:link_shared_paths' step.
-set :shared_paths, ['config/database.yml', 'log', 'config/application.yml']
+set :shared_paths, ['config/database.yml', 'log', 'config/secrets.yml']
 
 
+# This task is the environment that is loaded for most commands, such as
+# `mina deploy` or `mina rake`.
+task :environment do
+  queue %{echo "-----> Loading environment"}
+  invoke :'rvm:use[ruby-2.1.3-p242@social]'
+end
 
-task deploy: :environment do
+# Put any custom mkdir's in here for when `mina setup` is ran.
+# For Rails apps, we'll make some of the shared paths that are shared between
+# all releases.
+task :setup => :environment do
+  queue! %[mkdir -p "#{deploy_to}/shared/log"]
+  queue! %[chmod g+rx,u+rwx "#{deploy_to}/shared/log"]
+
+  queue! %[mkdir -p "#{deploy_to}/shared/config"]
+  queue! %[chmod g+rx,u+rwx "#{deploy_to}/shared/config"]
+
+  queue! %[touch "#{deploy_to}/shared/config/database.yml"]
+  queue  %[echo "-----> Be sure to edit 'shared/config/database.yml'."]
+
+  queue! %[touch "#{deploy_to}/shared/config/secrets.yml"]
+  queue %[echo "-----> Be sure to edit 'shared/config/secrets.yml'."]
+
+  # sidekiq needs a place to store its pid file and log file
+  queue! %[mkdir -p "#{deploy_to}/shared/pids/"]
+  queue! %[chmod g+rx,u+rwx "#{deploy_to}/shared/pids"]
+end
+
+desc "Deploys the current version to the server."
+task :deploy => :environment do
   deploy do
-    # Put things that prepare the empty release folder here.
-    # Commands queued here will be run on a new release directory.
+
+    # stop accepting new workers
+    invoke :'sidekiq:quiet'
 
     invoke :'git:clone'
+    invoke :'deploy:link_shared_paths'
     invoke :'bundle:install'
+    invoke :'rails:db_migrate'
+    invoke :'rails:assets_precompile'
 
-    # These are instructions to start the app after it's been prepared.
     to :launch do
-      queue 'touch tmp/restart.txt'
-    end
-
-    # This optional block defines how a broken release should be cleaned up.
-    to :clean do
-      queue 'log "failed deployment"'
+      invoke :'sidekiq:restart'
+      invoke :'unicorn:restart'
     end
   end
 end
-
-# For help in making your deploy script, see the Mina documentation:
-#
-#  - http://nadarei.co/mina
-#  - http://nadarei.co/mina/tasks
-#  - http://nadarei.co/mina/settings
-#  - http://nadarei.co/mina/helpers
