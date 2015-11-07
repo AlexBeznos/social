43b7f9fecfb133a4c4864d10ce47beea87dc104f
the-barm <funfundzwanzigsten@gmail.com>
2015-11-07 17:22:39 +0000

    Timer starts on video load, not page load + small fixes

--- app/assets/javascripts/popup_modal.js
+++ app/assets/javascripts/popup_modal.js
@@ -1,22 +1,30 @@
-$(document).ready(function(){
-    counter = 15;
-    text = $('#countdown').text();
+var myTimer = function() {
+  counter = 15;
+  text = $('#countdown').text();
+  $('#countdown').html(counter + text);
+  $('#countdown').css("visibility", "visible");
+  interval = setInterval(function() {
+    counter--;
     $('#countdown').html(counter + text);
-    $('#countdown').css("visibility", "visible")
-    interval = setInterval(function() {
-      counter--;
-      $('#countdown').html(counter + text);
-      if (counter == 0) {
-        $('#close-banner').css("visibility", "visible")
-        $('#countdown').css("visibility", "hidden")
-        clearInterval(interval);
-      }
-    }, 1000);
+    if (counter == 0) {
+      $('#close-banner').css("visibility", "visible");
+      $('#countdown').css("visibility", "hidden");
+      clearInterval(interval);
+    }
+  }, 1000);
+}
 
+$(document).ready(function(){
     $('#close-banner').click(function () {
         $('#modal').css( "display", "none" );
         $(".modalcontent").get(0).pause();
     });
+
+    if ($('video[name = "media"]').length) {
+      $(".modalcontent").get(0).canplay(myTimer());
+    } else {
+      myTimer();
+    }
 });
 
 
