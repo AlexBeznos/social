fb6b028d69f7fa8470911944b654b36a6ba68d90
Alex Beznos <beznosa@yahoo.com>
2015-11-23 18:14:53 +0200

    refactore a bit

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,10 @@
 class PagesController < ApplicationController
   include HighVoltage::StaticPage
 
-  before_action :throw_am_home, if: "params[:id] == 'home'"
+  before_action :throw_home_am, if: "params[:id] == 'home'"
 
   private
-  def throw_am_home
+  def throw_home_am
     params[:id] = 'home_am' if request.host == 'gofriends.am'
   end
 end
