141db563b023c1ac411865ad4cb2dbc7104a2d2d
Alex Beznos <beznosa@yahoo.com>
2016-01-12 16:26:23 +0200

    remove redundant migrations

--- db/migrate/20160105134001_add_redirect_url_to_messages.rb
+++
@@ -1,6 +0,0 @@
-#class AddRedirectUrlToMessages < ActiveRecord::Migration
-#  def change
-#    add_column :messages, :redirect_url, :string
-#  end
-#end
-#NOTE: IT`S FROM OTHER TASK FROM BRANCH "redirect" AND IT`S APPEARED HERE SOMEHOW
