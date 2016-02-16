23aa2d6fd4b2404886a9e5990e04985444fbd284
alexserj <alexserj95@gmail.com>
2016-02-16 16:36:16 +0200

    add s3 routs

--- app/uploaders/logo_uploader.rb
+++ app/uploaders/logo_uploader.rb
@@ -4,7 +4,7 @@ class LogoUploader < CarrierWave::Uploader::Base
   include CarrierWave::Compatibility::Paperclip
 
   def paperclip_path
-    ":rails_root/public/uploads/:id/:attachment/:style_:basename.:extension"
+    "/images/logos/:id/:style.:extension"
   end
 
   # Include RMagick or MiniMagick support:
@@ -12,8 +12,7 @@ class LogoUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
+  storage :aws
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
