ca7858d39be08436bf54c750e8016d916fa07386
Alex Beznos <beznosa@yahoo.com>
2016-02-22 12:47:24 +0200

    fix proper asset-host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -107,7 +107,8 @@ Rails.application.config.middleware.use ExceptionNotification::Rack,
 CarrierWave.configure do |config|
   config.storage    = :aws
   config.aws_bucket = ENV.fetch('AWS_BUCKET')
-  config.aws_acl    = 'public-read'
+  config.aws_acl    = :public_read
+  config.asset_host = 'http://gowifi-prod.s3.amazonaws.com/'
 
   config.aws_credentials = {
     access_key_id:     ENV.fetch('AWS_ACCESS_KEY_ID'),
