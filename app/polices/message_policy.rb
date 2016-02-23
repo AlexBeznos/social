9aff547dfb775fe7d0b767193aa0d96ecebbd39e
Alex Beznos <beznosa@yahoo.com>
2016-02-24 00:23:37 +0200

    remove scope from message policy

--- app/polices/message_policy.rb
+++ app/polices/message_policy.rb
@@ -1,10 +1,4 @@
 class MessagePolicy < ApplicationPolicy
-  class Scope < Scope
-    def resolve
-      scope.all
-    end
-  end
-
   def permitted_attributes
     [:social_network_id, :message, :message_link, :image, :subscription]
   end
