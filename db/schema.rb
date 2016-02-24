875680e60b33badda2c01f749ff68edbe5ce652a
Kostya Zolo <r0uder7@gmail.com>
2016-02-24 14:47:39 +0200

    fixed test build

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160209113356) do
+ActiveRecord::Schema.define(version: 20160224124440) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -233,6 +233,11 @@ ActiveRecord::Schema.define(version: 20160209113356) do
   end
 
   add_index "polls", ["place_id"], name: "index_polls_on_place_id", using: :btree
+
+  create_table "previews", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "sessions", force: true do |t|
     t.string   "session_id", null: false
