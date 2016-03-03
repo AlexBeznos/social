7013c1c31e36dec7b95f59d9cc2d15992c49d5cd
YanekDevelop <yan.summer23@gmail.com>
2016-03-03 06:16:28 +0200

    Adds sms auth

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160302155857) do
+ActiveRecord::Schema.define(version: 20160303034357) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -131,16 +131,6 @@ ActiveRecord::Schema.define(version: 20160302155857) do
     t.datetime "updated_at"
   end
 
-  create_table "gowifi_sms", force: true do |t|
-    t.string   "phone"
-    t.string   "code"
-    t.integer  "place_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "gowifi_sms", ["place_id"], name: "index_gowifi_sms_on_place_id", using: :btree
-
   create_table "menu_items", force: true do |t|
     t.string   "name"
     t.text     "description"
@@ -249,6 +239,13 @@ ActiveRecord::Schema.define(version: 20160302155857) do
 
   add_index "sessions", ["session_id"], name: "index_sessions_on_session_id", using: :btree
   add_index "sessions", ["updated_at"], name: "index_sessions_on_updated_at", using: :btree
+
+  create_table "sms_auths", force: true do |t|
+    t.string   "phone"
+    t.string   "code"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "social_network_icons", force: true do |t|
     t.integer  "place_id"
