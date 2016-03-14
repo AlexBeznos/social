3eac1ec8e8ea91da1b06b297f0a40daba2d2c717
YanekDevelop <yan.summer23@gmal.com>
2016-03-14 13:27:03 +0200

    Fix some misssentanious bugs

--- spec/policies/support/policy_shared.rb
+++ spec/policies/support/policy_shared.rb
@@ -1,6 +1,6 @@
 RSpec.shared_examples "visitor" do
   context "for visitor" do
-    let(:author_error){Pundit::NotAuthorizedError }
+    let(:author_error){ Pundit::NotAuthorizedError }
 
     it{expect{described_class.new(nil,record)}.to raise_error(author_error)}
   end
