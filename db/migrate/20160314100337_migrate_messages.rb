6509bd30e853e2e81ce808978307bcf11b6a7ef7
Alex Beznos <beznosa@yahoo.com>
2016-03-15 01:54:43 +0200

    bug

--- db/migrate/20160314100337_migrate_messages.rb
+++ db/migrate/20160314100337_migrate_messages.rb
@@ -4,16 +4,18 @@ class MigrateMessages < ActiveRecord::Migration
     Message.active.each do |message|
       p '_+_+_+_'
       p message.image.url
-      Auth.create!(
-        resource_type: "#{message.social_network.name.capitalize}Auth",
-        place: message.place,
-        redirect_url: message.place.redirect_url,
-        resource_attributes: {
-          message: message.message,
-          message_url: message.message_link,
-          remote_image_url: to_full_url(message.image.url)
-        }
-      )
+      unless message.id == 41
+        Auth.create!(
+          resource_type: "#{message.social_network.name.capitalize}Auth",
+          place: message.place,
+          redirect_url: message.place.redirect_url,
+          resource_attributes: {
+            message: message.message,
+            message_url: message.message_link,
+            remote_image_url: to_full_url(message.image.url)
+          }
+        )
+      end
     end
   end
 
