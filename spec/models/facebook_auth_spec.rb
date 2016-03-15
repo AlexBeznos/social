cf77ff0e805ad3cd1c4983892e5067ddd47c142d
YanekDevelop <yan.summer23@gmal.com>
2016-03-15 14:34:17 +0200

    Fix tests bug

--- spec/models/facebook_auth_spec.rb
+++ spec/models/facebook_auth_spec.rb
@@ -4,7 +4,6 @@ RSpec.describe FacebookAuth, :type => :model do
   it { is_expected.to have_one :auth}
   it { is_expected.to validate_presence_of(:image)}
   it { is_expected.to validate_presence_of(:message)}
-  it { is_expected.to validate_presence_of(:message_url)}
   it_behaves_like 'with url validation for', :message_url
 
   it "has proper name" do
