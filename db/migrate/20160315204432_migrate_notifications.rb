51316bd7c2e39ed9a0670027b20e7b5f66f0a70d
YanekDevelop <yan.summer23@gmal.com>
2016-03-16 01:51:21 +0200

    Add proper migration

--- db/migrate/20160315204432_migrate_notifications.rb
+++ db/migrate/20160315204432_migrate_notifications.rb
@@ -1,5 +1,16 @@
 class MigrateNotifications < ActiveRecord::Migration
   def self.up
+    user_ids = User.where(group: "general").pluck(:id)
+
+    Place.where(user_id: user_ids).each do |place|
+      place.auths.each do |auth|
+        Notification.create!(
+          category: "Unapproved authentication",
+          source: auth,
+          user: place.user.franchisee
+        )
+      end
+    end
   end
 
   def self.down
