354b645851881f2f1e5307f3ca9fe655a3629b5d
Alex Beznos <beznosa@yahoo.com>
2016-03-16 19:01:58 +0200

    fix specs partialy

--- app/models/customer/visit.rb
+++ app/models/customer/visit.rb
@@ -10,10 +10,10 @@ class Customer::Visit < ActiveRecord::Base
   belongs_to :network_profile, class_name: 'Customer::NetworkProfile',
                                foreign_key: :customer_network_profile_id
 
-  validates :network_profile, :place, presence: true, unless: 'by_password'
+  validates :network_profile, :place, presence: true, unless: :by_password
   # validate :ones_a_day_visit, unless: 'by_password'
 
-  after_create :calculate_reputation, unless: 'by_password'
+  after_create :calculate_reputation, unless: :by_password
 
   private
   def ones_a_day_visit
