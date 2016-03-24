00177523a194fe5337559b90ce9448d89abea701
Alex Beznos <beznosa@yahoo.com>
2016-03-24 23:56:45 +0200

    move customer name adding to network profile, refactore instagram

--- app/services/instagram_service.rb
+++ app/services/instagram_service.rb
@@ -1,6 +1,4 @@
-class InstagramService # NOTICE: Refactore class to general style in case of using
-  attr_accessor :hash
-
+class InstagramService
   def initialize(hash)
     @place = hash[:place]
     @message = hash[:message]
@@ -15,23 +13,11 @@ class InstagramService # NOTICE: Refactore class to general style in case of usi
     }
   end
 
-  def self.get_user_id(user_name)
-    Instagram.user_search(user_name, :count => 1).first['id']
-  end
+  def self.get_user_name(network_profile)
+    client = Instagram.client(access_token: network_profile.access_token)
+    user = client.user
 
-  def self.get_more_customer_info(customer)
-    network_profile = customer.network_profiles
-                              .where({:social_network => SocialNetwork.select(:id).find_by(name: 'vkontakte')})
-                              .first
-
-    user = get_user network_profile
-    full_name = user['full_name'].split(' ')
-
-    unless full_name.empty?
-      { first_name: full_name[0], last_name: full_name[1] }
-    else
-      { first_name: user['username'] }
-    end
+    user['full_name'].blank? ? user['username'] : user['full_name']
   end
 
   def self.get_friends_number(customer)
@@ -41,11 +27,5 @@ class InstagramService # NOTICE: Refactore class to general style in case of usi
   end
 
   def advertise
-  end
-
-  private
-  def self.get_user(network_profile)
-    client = Instagram.client(access_token: network_profile.access_token)
-    client.user
   end
 end
