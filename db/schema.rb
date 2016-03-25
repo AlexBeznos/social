3421692a5249bcf94afaf1e8d8d2afb392d7aa77
YanekDevelop <yan.summer23@gmal.com>
2016-03-25 13:47:08 +0200

    Add some stuff

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,8 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
-ActiveRecord::Schema.define(version: 20160320123257) do
 
+ActiveRecord::Schema.define(version: 20160322115447) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
