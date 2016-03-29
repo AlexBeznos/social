e6f5c940b1a85aff8a23b380bce93854c4d0cecc
Andrey Ryabko <honestua@gmail.com>
2016-03-29 17:45:57 +0300

    Implement preview

---
+++ app/controllers/gowifi_previews_controller.rb
@@ -0,0 +1,11 @@
+class GowifiPreviewsController < ApplicationController
+  layout 'gowifi_previews'
+
+  skip_after_action :verify_authorized
+
+  def show
+    @auth = Auth.find(params[:auth_id])
+    @provider = @auth.name.to_s
+    @place = Place.find_by_slug(params[:slug])
+  end
+end
