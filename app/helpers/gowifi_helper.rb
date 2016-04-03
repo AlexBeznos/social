1bf790a696fe4b6fe558f5ab1710d93963875762
YanekDevelop <yan.summer23@gmail.com>
2016-04-03 18:24:18 +0300

    Some bug fix

--- app/helpers/gowifi_helper.rb
+++ app/helpers/gowifi_helper.rb
@@ -1,7 +1,7 @@
 module GowifiHelper
   def network_path(place, auth)
     if place.post_preview && auth.name != Auth::NETWORKS[:instagram].to_sym
-      preview_path(place, auth: auth)
+      preview_path(place, auth_id: auth)
     else
       auth_path(place, auth)
     end
--- app/models/place.rb
+++ app/models/place.rb
@@ -77,7 +77,7 @@ class Place < ActiveRecord::Base
 
   def scratchcard_items_presence
     if scratchcard && menu_items.empty?
-      errors.add(:scratchcard, "models.errors.validations.scratchcard_no_items_error")
+      errors.add(:scratchcard, I18n.t("models.errors.validations.scratchcard_no_items_error"))
     end
   end
 
