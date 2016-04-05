971a775ab9d664cc3093746e9ff95c97085fc495
Andrey Ryabko <honestua@gmail.com>
2016-04-05 11:58:40 +0300

    Set up ahoy

--- config/initializers/ahoy.rb
+++ config/initializers/ahoy.rb
@@ -1,3 +1,12 @@
 class Ahoy::Store < Ahoy::Stores::ActiveRecordStore
-  # customize here
+  def visit_model
+    AhoyVisit
+  end
+
+  def track_visit(options)
+    super do |visit|
+      visit.place = Place.find_by_slug(controller.params[:slug])
+      visit.customer = controller.current_customer
+    end
+  end
 end
