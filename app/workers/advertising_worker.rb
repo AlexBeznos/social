7214195399399f133816278cfcedc2b0f63b9791
Sergey Ryabko <noto.rsv@gmail.com>
2016-04-05 16:58:43 +0300

    Fix typos

--- app/workers/advertising_worker.rb
+++ app/workers/advertising_worker.rb
@@ -4,15 +4,16 @@ class AdvertisingWorker
   sidekiq_options queue: :advertisment, retry: false, failures: true
 
   def perform(place_slug, auth_id, profile_id)
-    @place       = Place.find_by_slug(place_slug)
-    @auth        = Auth.find(auth_id)
-    @profile     = Profile.find(profile_id)
+    @place   = Place.find_by_slug(place_slug)
+    @auth    = Auth.find(auth_id)
+    @profile = Profile.find(profile_id)
 
-    post_advertisment
+    post_advertisement
   end
 
   private
-  def post_advertisment
+
+  def post_advertisement
     profile_type = @profile.resource_type.downcase.gsub(/profile/, '')
     service = "#{profile_type.capitalize}Service".constantize
     attrs   = {
