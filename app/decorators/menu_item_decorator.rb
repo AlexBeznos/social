0086061583641b239a5ce80cb3c6ccb6a59853d2
YanekDevelop <yan.summer23@gmail.com>
2016-04-06 15:26:33 +0300

    Add proper dicorator for menu items

---
+++ app/decorators/menu_item_decorator.rb
@@ -0,0 +1,30 @@
+class MenuItemDecorator
+
+  def self.decorate_multiple(menu_items)
+    menu_items.map do |item|
+      self.new(item)
+    end
+  end
+
+  def initialize(menu_item)
+    @menu_item = menu_item
+  end
+
+  def orders_today
+    @menu_item.orders.to_ary.count do |order|
+      order.updated_at.today?
+    end
+  end
+
+  def orders_by_week
+    @menu_item.orders.to_ary.count do |order|
+      order.created_at >= (Time.now - 7.days)
+    end
+  end
+
+  def orders_by_month
+    @menu_item.orders.to_ary.count do |order|
+      order.created_at >= (Time.now - 1.month)
+    end
+  end
+end
