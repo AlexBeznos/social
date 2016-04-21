8ed6867798f65f660cf612f714b28817b2daf003
Alex Beznos <beznosa@yahoo.com>
2016-04-21 21:46:29 +0300

    fix login page creator

--- app/workers/router/login_page_worker.rb
+++ app/workers/router/login_page_worker.rb
@@ -1,13 +1,14 @@
 require 'fileless_io'
 
 class Router::LoginPageWorker
+  include Rails.application.routes.url_helpers
   include Sidekiq::Worker
 
   sidekiq_options queue: :router_setup, failures: true
 
   def perform(router_id)
     router = Router.find(router_id)
-    gowifi_url = "http://wifi.gofriends.com.ua#{gowifi_place_path(Place.last)}"
+    gowifi_url = "http://wifi.gofriends.com.ua#{gowifi_place_path(router.place)}"
     login_path = File.join(Rails.root, 'vendor/router/samples/login.html')
     login_io = File.open(login_path).read
 
