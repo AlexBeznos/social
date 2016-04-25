0851cb1eb182b53407c5a7a375a15acdf247e616
Alex Beznos <beznosa@yahoo.com>
2016-04-25 07:28:21 +0300

    router fixtures

--- app/workers/wifi_router/login_page_worker.rb
+++ app/workers/wifi_router/login_page_worker.rb
@@ -9,7 +9,7 @@ class WifiRouter::LoginPageWorker
   def perform(router_id)
     router = Router.find(router_id)
     gowifi_url = "http://wifi.gofriends.com.ua#{gowifi_place_path(router.place)}"
-    login_path = File.join(Rails.root, 'vendor/router/samples/login.html')
+    login_path = File.join(Rails.root, 'vendor', 'router', 'samples', 'login.html')
     login_io = File.open(login_path).read
 
     login_io.gsub! /##LOGIN_PAGE##/, gowifi_url
