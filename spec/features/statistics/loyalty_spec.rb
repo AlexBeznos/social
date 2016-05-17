05dcee02358c0086db7bdd72073533395df6a795
YanekDevelop <yan.summer23@gmail.com>
2016-05-18 01:39:36 +0300

    Some test fixes

--- spec/features/statistics/loyalty_spec.rb
+++ spec/features/statistics/loyalty_spec.rb
@@ -1,12 +1,11 @@
 require 'rails_helper'
 
 describe "loyalty statistic" do
-    let(:user){ create :user }
+    let(:user){ create :user_general }
     let(:place){ create :place, user: user }
     let(:today_order){ create :order, place: place }
     let(:last_week_order){  create :last_week_order, place: place }
     let(:last_month_order){  create :last_month_order, place: place }
-
 
     before(:each)do
       login(user)
@@ -16,7 +15,7 @@ describe "loyalty statistic" do
       today_order.customer.reputations << create(:reputation, place: place)
 
       visit place_statistics_loyalty_path(place)
-      click_link("Today")
+      click_link(I18n.t("statistics.loyalty.today"))
 
       expect(page).to have_selector("#order_#{today_order.id}")
     end
@@ -25,7 +24,7 @@ describe "loyalty statistic" do
       last_week_order.customer.reputations << create(:reputation, place: place)
 
       visit place_statistics_loyalty_path(place)
-      click_link("This week")
+      click_link(I18n.t("statistics.loyalty.this_week"))
 
       expect(page).to have_selector("#order_#{last_week_order.id}")
     end
@@ -34,10 +33,9 @@ describe "loyalty statistic" do
       last_month_order.customer.reputations << create(:reputation, place: place)
 
       visit place_statistics_loyalty_path(place)
-      click_link("This month")
+      click_link(I18n.t("statistics.loyalty.this_month"))
 
       expect(page).to have_selector("#order_#{last_month_order.id}")
-
     end
 
     describe "date piker", js: true do
@@ -47,7 +45,8 @@ describe "loyalty statistic" do
         month_ago = (Time.now - 1.month).day
 
         visit place_statistics_loyalty_path(place)
-        click_button("Custom date")
+        I18n.locale = :en
+        click_button(I18n.t("statistics.loyalty.custom_date"))
 
         find("#from").click
         find("a[title='Prev']").click
