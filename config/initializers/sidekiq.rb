3407796739494facbba9a94bcb23b64528a81bed
Alex Beznos <beznosa@yahoo.com>
2016-05-25 15:12:48 +0300

    fix sidekiq cron

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -6,4 +6,4 @@ Sidekiq.configure_client do |config|
   config.redis = { url: "redis://#{ENV['REDIS_URL']}/", namespace: 'sidekiq', network_timeout: 5 }
 end
 
-Sidekiq::Cron::Job.create(name: 'Everyday database backup.', cron: '@daily', class: 'BackupWorker')
+Sidekiq::Cron::Job.create(name: 'Everyday database backup.', cron: '00 00 * * *', class: 'BackupWorker')
