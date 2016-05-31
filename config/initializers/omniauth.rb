59052e6dd96479dfbd9f22658e9754d129a674c4
Alex Beznos <beznosa@yahoo.com>
2016-05-31 19:14:21 +0300

    fix facebook info

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -11,6 +11,6 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV['FB_APP_ID'], ENV['FB_APP_SECRET'],
   {
     :scope => 'publish_actions,user_friends',
-    :info_fields => 'gender,link'
+    :info_fields => 'gender,link,first_name,last_name'
   }
 end
