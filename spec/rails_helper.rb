3206e04c2316705568ea8d03bcad8140afbef69b
Alex Beznos <beznosa@yahoo.com>
2016-06-29 16:45:30 +0300

    refactore network auth feature spec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -51,18 +51,16 @@ RSpec.configure do |config|
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
+
+  config.before(:each) do
+    Sidekiq::Worker.clear_all
+  end
 end
 
 Shoulda::Matchers.configure do |config|
   config.integrate do |with|
     with.test_framework :rspec
     with.library :rails
-  end
-end
-
-RSpec.configure do |config|
-  config.before(:each) do
-    Sidekiq::Worker.clear_all
   end
 end
 
