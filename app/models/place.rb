c65805ff0ea3c6b2b3ca45ddd00cfb576d726ee6
YanekDevelop <yan.summer23@gmail.com>
2016-07-01 12:39:02 +0300

    Update place.rb

--- app/models/place.rb
+++ app/models/place.rb
@@ -34,7 +34,7 @@ class Place < ActiveRecord::Base
   validates :logo, file_content_type: { allow: ["image/jpeg", "image/png", "image/gif"] },
                    file_size: { less_than_or_equal_to: 10.megabytes }
 
-  validates :save_device, absence: true, if: 'self.mfa?'
+  validates :save_device, absence: true, if: 'mfa?'
 
   after_validation :geocode, if: :city_changed?
   after_commit :setup_router, on: :create
