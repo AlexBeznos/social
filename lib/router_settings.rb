cc048e89be08c565fd63bf6102100d00eb90ba5c
Alex Beznos <beznosa@yahoo.com>
2016-08-16 13:52:08 +0300

    add new mikrotik settings

--- lib/router_settings.rb
+++ lib/router_settings.rb
@@ -5,14 +5,23 @@ class RouterSettings
   SETTINGS_FILE = 'settings.zip'
   FILES_TO_COPY = [
     'config.rsc',
-    'GoFriends.exe',
-    'preconfig.rsc',
-    'README.txt'
+    'plink.exe',
+    'psftp.exe',
+    'psftp.scr',
+    'start.bat',
+    'y.txt',
+    'README.html'
+  ]
+
+  HOTSPOT_FILES_TO_COPY = [
+    'errors.txt',
+    'logout.html',
+    'redirect.html'
   ]
 
   def initialize(router)
     @router = router
-    create_settings_folder
+    create_settings_folders
   end
 
   def zip
@@ -21,12 +30,18 @@ class RouterSettings
   end
 
   def copy_files
-    login_path = File.join(settings_path, @router.login_page.file.filename)
+    login_path = File.join(settings_path, 'hotspot', @router.login_page.file.filename)
     File.open(login_path, 'w') { |file| file.write(@router.login_page.read) }
 
     FILES_TO_COPY.each do |file|
       path = File.join(default_settings_path, file)
       FileUtils.cp(path, settings_path)
+    end
+
+    HOTSPOT_FILES_TO_COPY.each do |file|
+      path_from = File.join(default_settings_path, 'hotspot', file)
+      path_to = File.join(settings_path, 'hotspot')
+      FileUtils.cp(path_from, path_to)
     end
   end
 
@@ -42,7 +57,7 @@ class RouterSettings
     #  end
     #end
 
-    cmnds.push(ovpn_command)
+    # cmnds.push(ovpn_command)
     File.open(config_path, 'w') { |file| file.write(cmnds.join("\n")) }
   end
 
@@ -57,13 +72,14 @@ class RouterSettings
 
   private
 
-  def create_settings_folder
+  def create_settings_folders
     settings_dir_path = File.join(Rails.root, 'public', 'settings')
 
     Dir.mkdir(settings_dir_path) unless File.directory?(settings_dir_path)
     FileUtils.rm_rf(settings_path) if File.directory?(settings_path)
 
     Dir.mkdir(settings_path)
+    Dir.mkdir(File.join(settings_path, 'hotspot'))
   end
 
   def settings_path
@@ -74,19 +90,19 @@ class RouterSettings
     File.join(Rails.root, 'vendor', 'router', 'settings')
   end
 
-  def fetch_command(crt)
-    "/tool fetch url=http://wifi.gofriends.com.ua/routers/#{@router.access_token}/#{crt} dst-path=#{crt} mode=http\n:delay 5"
-  end
-
-  def import_command(crt)
-    "/certificate import file-name=#{crt} passphrase=\"\""
-  end
-
-  def remove_command(crt)
-    "/file remove #{crt}"
-  end
-
-  def ovpn_command
-    "/interface ovpn-client add name=\"ovpn\" connect-to=#{ENV['OVPN_SERVER']} port=443 user=\"#{@router.place.slug}\" password=\"\" disabled=no profile=default certificate=\"cert_2\" add-default-route=no"
-  end
+  # def fetch_command(crt)
+  #   "/tool fetch url=http://wifi.gofriends.com.ua/routers/#{@router.access_token}/#{crt} dst-path=#{crt} mode=http\n:delay 5"
+  # end
+  #
+  # def import_command(crt)
+  #   "/certificate import file-name=#{crt} passphrase=\"\""
+  # end
+  #
+  # def remove_command(crt)
+  #   "/file remove #{crt}"
+  # end
+  #
+  # def ovpn_command
+  #   "/interface ovpn-client add name=\"ovpn\" connect-to=#{ENV['OVPN_SERVER']} port=443 user=\"#{@router.place.slug}\" password=\"\" disabled=no profile=default certificate=\"cert_2\" add-default-route=no"
+  # end
 end
